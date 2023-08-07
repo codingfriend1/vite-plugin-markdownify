f57a99b393a469072979ebd59415d852da147c31
Jon Paul Miles <milesjonpaul@gmail.com>
2023-08-07 11:54:09 -0500

    First commit.

---
+++ README.md
@@ -0,0 +1,90 @@
+# vite-plugin-markdownify
+
+`vite-plugin-markdownify` is a Vite plugin for converting Markdown files into HTML pages using a template. It also injects metadata and static site navigation data into your application. It's a simple tool for generating static websites with Markdown and Vite.
+
+## Features
+
+- **Markdown to HTML Conversion**: Easily convert Markdown files into HTML pages.
+- **Metadata Extraction**: Extract front matter and file statistics, including creation and modification dates.
+- **Navigation Data Injection**: Inject page and site navigation data into the `window.markdownify` object.
+- **Markdown Transformations**: Apply transformations using markdown-it and plugins.
+- **Custom Templates**: Use a custom HTML template for page generation.
+- **Live Updates**: Update the template live during development.
+- **Draft Ignoring**: Ignore draft pages during processing.
+- **Sitemap & Feed Generation**: Automatically generate `sitemap.xml` and `feed.xml`.
+
+## Installation
+
+```bash
+npm install --save-dev vite-plugin-markdownify
+# or
+yarn add vite-plugin-markdownify
+```
+
+## Usage
+
+```javascript
+import { defineConfig } from 'vite';
+import markdownify from 'vite-plugin-markdownify';
+
+export default defineConfig({
+  plugins: [
+    markdownify({
+      template: './path/to/template.html',
+      input: './path/to/markdown',
+      output: './path/to/output',
+      contentPlaceholder: `<!--markdownify content-->`,
+      metaPlaceholder: `<!--markdownify meta-->`,
+      words_per_minute: 275, // Optional reading time calculation
+      defaults: { // Default meta tags and fields for `feed.xml`
+        title: `Default Title`,
+        author: `Author's Name`,
+        description: 'Site description',
+        baseUrl: `https://yoursiteexample.com`,
+        "og:type": `article`
+      }
+    })
+  ]
+});
+```
+
+## Configuration
+
+- **`template`**: HTML template path (default `'./index.html'`).
+- **`input`**: Markdown files directory (default `'./markdown'`).
+- **`output`**: Output directory for HTML files (default `process.cwd()`).
+- **`contentPlaceholder`**: Placeholder for content script (default `<!--markdownify content-->`).
+- **`metaPlaceholder`**: Placeholder for meta tags (default `<!--markdownify meta-->`).
+- **`words_per_minute`**: Optional reading time calculation (recommended `275`) presented in `readingTime` (`int` of minutes).
+- **`defaults`**: Values to be merged with page meta tags. Required for `feed.xml`, must include `title`, `author`, `baseUrl`, and `description` for proper rendering.
+
+## Front Matter
+
+Include YAML front matter in Markdown files for title, description, keywords, update and creation dates, and draft status.
+
+```md
+---
+title: "Page title"
+description: "Page description"
+keywords: ['best site ever', 'all about penguins'].
+updatedAt: str | int timestamp 
+createdAt: str | int timestamp
+draft: false
+---
+```
+
+- `createdAt`: is the creation date of the page. If this is not provided, the plugin will use the file's birth date.
+- `updatedAt`: is the last update date of the page. If this is not provided, the plugin will use the file's modification date.
+- `draft`: If this is true, the plugin will ignore this file during processing.
+
+## Contribution & Contact
+
+For issues or suggestions, please open an issue or submit a pull request. Feel free to contact us with any questions or suggestions.
+
+## Disclaimer
+
+This Software is provided "as is", without warranty. Users assume all risks and responsibilities. The author(s) and maintainer(s) will not be liable for damages or losses.
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,307 @@
+import MarkDownIt from 'markdown-it';
+import matter from "gray-matter";
+import fs from 'fs-extra';
+import path from 'path';
+import attrs from 'markdown-it-attrs';
+import markdownToken from 'markdown-it-modify-token';
+import markdownFootnote from 'markdown-it-footnote';
+import { URL } from 'url';
+
+// Define the placeholders and directory paths
+const MARKDOWNIFY_CONTENT_PLACEHOLDER = '<!--markdownify content-->'
+const MARKDOWNIFY_META_PLACEHOLDER = '<!--markdownify meta-->'
+const DEFAULT_TEMPLATE_PATH = './index.html'
+const DEFAULT_MARKDOWN_DIR = path.resolve(__dirname, './markdown');
+const DEFAULT_OUTPUT_DIR = process.cwd();
+const markdownIt = createMarkdownItInstance();
+let WORDS_PER_MINUTE = null;
+
+// Define the tags with meta data name
+const META_TAGS_WITH_NAME = [
+  'owner', 'author', 'application-name', 'generator', 'referrer', 'theme-color',
+  'copyright', 'medium', 'language', 'description', 'keywords', 'robots', 'viewport'
+];
+
+const markdownify = (options = {}) => {
+
+  const { input, output, defaults, contentPlaceholder, metaPlaceholder, words_per_minute, template } = options;
+  const markdownDir = input || DEFAULT_DIRS.markdown;
+  const outputDir = output || DEFAULT_DIRS.output;
+  const markdownify_content_placeholder = contentPlaceholder || MARKDOWNIFY_CONTENT_PLACEHOLDER;
+  const markdownify_meta_placeholder = metaPlaceholder || MARKDOWNIFY_META_PLACEHOLDER;
+  WORDS_PER_MINUTE = words_per_minute
+
+  return {
+    name: 'vite-plugin-markdownify',
+    // apply: 'all', // Apply this plugin during both development and production
+
+    async writeBundle() {
+      const templatePath = template || DEFAULT_TEMPLATE_PATH;
+      console.log(`templatePath`, templatePath);
+      await renderMarkdownFiles(templatePath, markdownDir, outputDir, defaults, markdownify_content_placeholder, markdownify_meta_placeholder);
+    },
+
+    transformIndexHtml: {
+      enforce: 'pre',
+      transform: (html, { path: url }) => {
+
+        if (process.env.NODE_ENV !== 'development') {
+          return html;
+        }
+
+        const pages = getFiles(markdownDir)
+          .map(file => processFile(file, markdownDir, defaults))
+          .filter(file => file)
+          .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+
+        const page = pages.find(p => p.filename === 'index')
+
+        return substituteHtml(html, page, pages, markdownify_content_placeholder, markdownify_meta_placeholder)
+      }
+    }
+  };
+};
+
+// Function to render all markdown files in a directory
+async function renderMarkdownFiles(templatePath, markdownDir, outputDir, defaults, markdownify_content_placeholder, markdownify_meta_placeholder) {
+
+  const template = fs.readFileSync(templatePath, 'utf-8'); // Read the HTML template
+
+  const pages = getFiles(markdownDir)
+    .map(file => processFile(file, markdownDir, defaults))
+    .filter(file => file)
+    .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+
+  pages
+    .map(page => ({
+      ...page,
+      output: substituteHtml(template, page, pages, markdownify_content_placeholder, markdownify_meta_placeholder)
+    }))
+    .map(page => makeFile(`${page.filename}.html`, outputDir, page.output))
+
+  makeSitemap(pages, outputDir)
+  makeFeed(pages, outputDir, defaults)
+}
+
+const formatDate = date => new Date(date).getTime(); // Function to format date
+
+// Function to process a markdown file into HTML
+function processFile(file, markdownDir, defaults) {
+  const fileInfo = matter(fs.readFileSync(file, 'utf-8')); // Read the markdown file
+
+  if (fileInfo.data.draft) { // If it is a draft, don't process it
+    return false;
+  }
+
+  const stats = fs.statSync(file); // Get file statistics
+  const filename = path.relative(markdownDir, file).replace(/\.md$/, '') // Get filename
+  const html = markdownIt.render(fileInfo.content)
+
+  let mergeReadingTime = {}
+
+  if(WORDS_PER_MINUTE) {
+    mergeReadingTime.readingTime = calculateReadingTime(html)
+  }
+
+  return { 
+    ...defaults,
+    ...fileInfo.data,
+    updatedAt: formatDate(fileInfo.data.updated ? fileInfo.data.updated : stats.mtime),
+    createdAt: formatDate(fileInfo.data.created),
+    absolute_url: new URL(fileInfo.data.url || filename, defaults.baseUrl).href,
+    ...mergeReadingTime,
+    filename,
+    url: fileInfo.data.url || "/" + filename,
+    html
+  }; // Return processed page info
+}
+
+/**
+ * Counts the number of words in an html string
+ * @param {string} html Expected to be the rendered html string from a markdown file
+ * @return {number} Number of whole words in the html string
+ */
+function get_word_count(html) {
+  let words = html.replace(/<[^>]*>/g," ");
+  words = words.replace(/\s+/g, ' ');
+  words = words.trim();
+
+  return words.split(" ").length
+}
+
+/**
+ * Returns the number of minutes an average reader would need to read a given number of words
+ * @param {number} word_count The number of words to read
+ * @return {number} The number of minutes it would take an average reader to read
+ */
+function get_reading_time(word_count) {
+
+  const reading_time_in_minutes = Math.round(word_count / WORDS_PER_MINUTE)
+
+  return reading_time_in_minutes
+}
+
+
+function calculateReadingTime(html) {
+  const wordCount = get_word_count(html);
+  return get_reading_time(wordCount);
+}
+
+function substituteHtml(template, page, pages, markdownify_content_placeholder, markdownify_meta_placeholder) {
+
+  const htmlInsert = `<script>
+    window.markdownify = { 
+      page: ${JSON.stringify(page)}, 
+      pages: ${JSON.stringify(pages)} 
+    }
+  </script>`
+
+  const metaInsert = Object.keys(page)
+    .map(key => createMetaTag(page, key))
+    .join('');
+
+  return template
+    .replace(markdownify_content_placeholder, htmlInsert)
+    .replace(markdownify_meta_placeholder, metaInsert)
+}
+
+function makeSitemap(pages, outputDir) {
+
+  const sitemap_template = `<?xml version="1.0" encoding="utf-8" ?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"><!-- Urls will be auto injected -->
+</urlset>`;
+
+  const sitemap_string = pages
+    .map(file => {
+      return `
+  <url>
+    <loc>${file.absolute_url}</loc>
+    <priority>1.0</priority>
+    <lastmod>${new Date(file.updatedAt).toISOString()}</lastmod>
+  </url>`})
+    .join("");
+
+  let sitemap_html = sitemap_template.replace(
+    "<!-- Urls will be auto injected -->",
+    sitemap_string
+  );
+
+  makeFile(`sitemap.xml`, outputDir, sitemap_html)
+}
+
+function makeFeed(pages, outputDir, defaults) {
+
+  const feed_template = `<?xml version="1.0" encoding="utf-8" ?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+  <channel>
+    <title>${defaults.title}</title>
+    <author>${defaults.author}</author>
+    <link>${defaults.baseUrl}</link>
+    <description>${defaults.description}</description>
+    <atom:link href="${defaults.baseUrl}/feed.xml" rel="self" type="application/rss+xml"></atom:link><!-- Urls will be auto injected -->
+  </channel>
+</rss>`;
+
+  const feed_string = pages
+    .filter(file => !['home', 'index', 'privacy-policy', 'about', 'contact', 'analytics', 'missing', '404'].includes(file.filename))
+    .filter(file => file.draft !== true)
+    .map(file => {
+      return `
+      <item>
+        <title>${file.title}</title>
+        <author>${file.author || defaults.author}</author>
+        <pubdate>${new Date(file.createdAt).toISOString()}</pubdate>
+        <description>${file.description || defaults.description}</description>
+        <link>${file.absolute_url}</link>
+        <guid ispermalink="true">${file.absolute_url}</guid>
+      </item>`;
+    })
+    .join("");
+
+  let feed_xml = feed_template.replace(
+    "<!-- Urls will be auto injected -->",
+    feed_string
+  );
+
+  makeFile(`feed.xml`, outputDir, feed_xml)
+}
+
+function makeFile(filename, outputDirectory, content) {
+  const outputPath = path.join(outputDirectory, filename);
+  fs.ensureDirSync(path.dirname(outputPath)); // Create directories if they don't exist
+  fs.writeFileSync(outputPath, content); // Write HTML to output file
+}
+
+// Function to create a meta tag based on key and content
+function createMetaTag(page, key) {
+
+  const content = page[key]
+
+  const dismiss = [
+    'html',
+    'url',
+    'baseUrl',
+    'created',
+    'updated',
+    'draft',
+    'filename',
+    'readingTime',
+  ]
+
+  if(dismiss.includes(key)) { return ''; }
+
+  // Define templates for creating meta tags based on key
+  const templates = {
+    title: () => `
+    <title>${content}</title>
+    <meta name="pagename" content="${content}" />
+    <meta property="og:title" content="${content}" />
+    <meta name="twitter:title" content="${content}" />`,
+    author: () => `
+    <meta name="author" content="${content}" />
+    <meta property="article:author" content="${content}" />`,
+    description: () => `
+    <meta name="description" content="${content}" />
+    <meta property="og:description" content="${content}" />
+    <meta name="twitter:description" content="${content}" />`,
+    absolute_url: () => `
+    <meta name="url" content="${content}" />
+    <meta property="og:url" content="${content}" />
+    <meta name="identifier-URL" content="${content}" />`,
+    updatedAt: () => `
+    <meta property="article:modified_time" content="${new Date(content).toISOString()}" />`,
+    createdAt: () => `
+    <meta property="article:published_time" content="${new Date(content).toISOString()}" />`,
+    default: () => META_TAGS_WITH_NAME.includes(key) ? `
+    <meta name="${key}" content="${content}" />`: `
+    <meta property="${key}" content="${content}" />`
+  };
+
+  return (templates[key] || templates.default)();
+}
+
+function getFiles(dirPath, files_ = []) {
+  const files = fs.readdirSync(dirPath); // Read all files in directory
+  for (let file of files) { // Iterate through all files
+    let name = path.join(dirPath, file);
+    if (fs.statSync(name).isDirectory()) { // If the file is a directory, recurse
+      getFiles(name, files_);
+    } else {
+      // Only add the file to the list if it has a .md extension
+      if (path.extname(file) === '.md') {
+        files_.push(name); // If the file is not a directory, add it to the list
+      }
+    }
+  }
+  return files_; // Return the list of files
+}
+
+// Function to create an instance of markdownIt with required options and plugins
+function createMarkdownItInstance() {
+  return MarkDownIt({ html: true, linkify: false, typographer: false })
+    .use(attrs)
+    .use(markdownToken)
+    .use(markdownFootnote);
+}
+
+export default markdownify;
